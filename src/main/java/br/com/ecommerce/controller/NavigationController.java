5ec462fb1b47b1766a56418cb6c73251e5629fff
Gabriel Freire <gabrbfreire@gmail.com>
2020-11-22 19:10:07 -0300

    Thymeleaf config, landing page and error page

--- src/main/java/br/com/ecommerce/controller/NavigationController.java
+++ src/main/java/br/com/ecommerce/controller/NavigationController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GreetingController {
+public class NavigationController {
 
+    @GetMapping("")
+    public String index(){
+        return "main";
+    }
 }
