ce32156da1e5143cb1f4b262e3585cb917a6986b
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-03 21:55:26 -0300

    Implemented getProductsByCategory

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,26 +1,15 @@
-/* $.ajax({
-  type: "POST",
-  url: "boards",
-  data: {boardName: boardName},
-  success:function() {
-      getBoards();
-      $('#board-name').val('');
-  }
-}); */
-
 let categoriesHTML = "";
 let productsHTML = "";
 
 
-
 //Gets products and creates cards
-function getProductsByCategory() {
+function getProductsByCategory(category = 1) {
   $(document).ready(function () {
     $.ajax({
       type: "GET",
       url: "getProductsByCategoryId",
       data: {
-        categoryId: 1
+        categoryId: category
       },
       success: (data) => makeProductsHTML(data)
     })
@@ -28,6 +17,8 @@ function getProductsByCategory() {
 }
 
 function makeProductsHTML(data) {
+  $('#products-cards').empty();
+  productsHTML = "";
   data.forEach(element => {
     console.log(element);
     productsHTML += '<div class="col-lg-4 col-md-6 mb-4" id="productId=' + element.product_id + '"><div class="card h-100"><a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a><div class="card-body"><h4 class="card-title"><a href="#">' + element.product_name + '</a></h4><h5>$' + element.product_price + '</h5><p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p></div><div class="card-footer"><small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small></div></div></div>';
@@ -35,7 +26,6 @@ function makeProductsHTML(data) {
   //add product description and photo
   $('#products-cards').append(productsHTML);
 }
-
 
 
 //Gets categories and creates categories list
@@ -51,12 +41,14 @@ function getCategories() {
 
 function makeCategoriesHTML(data) {
   data.forEach(element => {
-    categoriesHTML += '<li class="categories-item list-group-item p-2" id="categoryId=' + element.categories_id + '">' + element.categories_title + '</li>';
+    categoriesHTML += '<li class="categories-item list-group-item p-2" id="categoryId' + element.categories_id + '">' + element.categories_title + '</li>';
   });
   $('#categories-list').append(categoriesHTML);
+  data.forEach(element => {
+    let itemId = "#categoryId" + element.categories_id;
+    $(itemId).click(() => getProductsByCategory(element.categories_id));
+  });
 }
 
 getCategories();
 getProductsByCategory();
-
-
