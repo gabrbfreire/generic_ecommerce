57f10cf3a3f078d5ccd8c1ddaff7327bd4852b31
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-10 19:11:52 -0300

    signin page

---
+++ src/main/webapp/js/signin.js
@@ -0,0 +1,30 @@
+console.log("a")
+
+$('#register-form').on('submit', function (e) {
+  e.preventDefault();
+
+  let name = $('#user-name').val();
+  let email = $('#user-email').val();
+  let password = $('#user-password').val();
+  let passwordRepeat = $('#user-repeat-password').val();
+
+  if (password == passwordRepeat) {
+    register(name, email, password);
+  } else {
+    document.getElementById('result').innerHTML = "Passwords do not match";
+  }
+});
+
+function register(name, email, password) {
+  $.ajax({
+    type: "POST",
+    url: "signup",
+    data: { name: name, email: email, password: password },
+    success: function () {
+      window.location.href = 'cart'
+    },
+    error: function () {
+      $('#result').text('User already exists');
+    }
+  })
+}
