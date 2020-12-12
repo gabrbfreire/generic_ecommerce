f990cf02a50e95e4c2ce101c84511fa766243a95
Gabriel Freire <gabrbfreire@gmail.com>
2020-12-12 20:07:35 -0300

    Cart total and checkout button frontend

--- src/main/webapp/js/cart.js
+++ src/main/webapp/js/cart.js
@@ -1,10 +1,13 @@
 function makeProductsHTML(data) {
   let productsHTML = "";
+  let cartTotalPrice = 0;
   $('#products-cards').empty();
   data.forEach(element => {
-    productsHTML += ' <div class="col-12 mb-4" id="productId=' + element.cart + '"><div class="card h-100"><div class="row"><a href="product?productId=' + element.product_id + '" class="col-12 col-sm-5 mh-50"><img class="card-img-top h-100 w-100" src="http://placehold.it/300x100" alt=""></a ><div class="card-body col-12 col-sm-5"><h4 class="card-title"><a href="product?productId=' + element.product_id + '">' + element.product_name + '</a></h4><h5>$' + element.product_price + '</h5></div><div class="col-2"><button type="button" class="btn btn-danger align-middle">Remove</button></div></div ></div ></div>';
+    productsHTML += '<div class="col-12 mb-4" id="productId=' + element.cart + '"><div class="card h-100"><div class="row"><a href="product?productId=' + element.product_id + '" class="col-12 col-sm-5 mh-50"><img class="card-img-top h-100 w-100" src="http://placehold.it/300x100" alt=""></a ><div class="card-body col-12 col-sm-5"><h4 class="card-title"><a href="product?productId=' + element.product_id + '">' + element.product_name + '</a></h4><h5>$' + element.product_price + '</h5></div><div class="col-2"><button type="button" class="btn btn-danger align-middle">Remove</button></div></div ></div ></div>';
+    cartTotalPrice += element.product_price;
   });
   //add product description and photo
+  $('#cart-total').html('Total: $' + cartTotalPrice);
   $('#products-cards').append(productsHTML);
 }
 
--- src/main/webapp/js/getSession.js
+++ src/main/webapp/js/getSession.js
@@ -5,7 +5,7 @@ $.ajax({
   url: "getSession",
   success: (data) => {
     $('#navbar').empty();
-    $('#navbar').append('<li class="nav-item"><a class="nav-link" href="signin">' + data + '</a></li><li class="nav-item"><a class="nav-link" href="logout">Logout</a></li><li class="nav-item"><a class="nav-link" href="cart">Cart: 1 Item</a></li>');
+    $('#navbar').append('<li class="nav-item"><a class="nav-link" href="#">' + data + '</a></li><li class="nav-item"><a class="nav-link" href="logout">Logout</a></li><li class="nav-item"><a class="nav-link" href="cart">Cart: 1 Item</a></li>');
     userData = data;
   },
 });
